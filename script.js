b383f32a10eee73326bcb878bd38e41434443f5b
Антон Адушкин <antonaduskin@MacBook-Pro-Anton.local>
2026-02-04 17:42:00 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Выбираем все кнопки
+const buttons = document.querySelectorAll('.toggle');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+        content.classList.toggle('open');
+    });
+});
